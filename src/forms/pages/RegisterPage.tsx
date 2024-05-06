1ee1576d0852dd5934d328ee4cc29cdaa8e56216
caesarals <slac_@hotmail.com>
2024-05-05 23:59:36 -0400

    First Commit

---
+++ src/forms/pages/RegisterPage.tsx
@@ -0,0 +1,29 @@
+
+export const RegisterPage = () => {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <form action="" method="">
+                <input 
+                    type="text"
+                    placeholder="Name" 
+                />
+                <input 
+                    type="email"
+                    placeholder="email" 
+                />
+                <input 
+                    type="password"
+                    placeholder="Password" 
+                />
+                <input 
+                    type="password"
+                    placeholder="Repeat Password" 
+                />
+
+                <button type="submit">Create</button>
+
+            </form>
+        </div>
+    )
+}
